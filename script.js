b9d7b3ad011c094b4df9cef517724b22a7d3ef57
Abderahim <ita27rmp100@gmail.com>
2025-02-10 12:12:33 +0100

    setting up the selection bar of logSign page

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// selection bar 
+let moods = ['login','signup'] , indexMode = 1
+function toogleMood(){
+    // chosen
+    $(`#${moods[Math.abs(indexMode-1)]}`).addClass('btn-outline-dark')
+    $(`#${moods[Math.abs(indexMode-1)]}`).removeClass('btn-light')
+    // replaced one
+    $(`#${moods[indexMode]}`).addClass('btn-light')
+    $(`#${moods[indexMode]}`).removeClass('btn-outline-dark')
+    indexMode = Math.abs(indexMode-1)
+}
+
+
 // edit on navbar according to title
 $(document).ready(
     function(){
+        $("label").addClass("btn p-3 active col-5")
         if((document.title).includes("chating") == false){
             $(".headForms").remove()
         }
