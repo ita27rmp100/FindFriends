57cb0ee656a39a2d2f65d9957f59576272d12da0
Abderahim <ita27rmp100@gmail.com>
2025-02-11 12:27:01 +0100

    rebuild the project with new plan

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ let mysql = require("mysql")
 let session = require("express-session")
 
 var indexRouter = require('./routes/index');
-var userRouter = require("./routes/logSign");
 const { hostname } = require('os');
 
 var app = express();
@@ -36,7 +35,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user',userRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/logSign.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/:mood?', function(req, res, next) {
-  res.render('logSign');
-});
-
-module.exports = router;
