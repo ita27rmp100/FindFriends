ce8381265bb95064b95127b40d67f0a5cd93ad7a
Abderahim <ita27rmp100@gmail.com>
2025-06-13 19:08:24 +0100

    start with new methode and strategy

--- db.sql
+++ db.sql
@@ -1,8 +1,16 @@
-create database template_db ;
-use template_db ;
-create table users(
-    username varchar(255) primary key,
-    password varchar(255)
+-- Creation
+create database ChatAno;
+use ChatAno;
+create table visitors(
+    username varchar(30) primary key,
+    img varchar(64),
+    gender enum('male','female'),
+    country varchar(20),
+    link varchar(255),
+    langs varchar(255), 
+    topics varchar(255)
 );
-
-select * from users;
+-- Delete the undefined elements ( in case there is an error )
+delete from visitors where username = "undefined";
+-- To test
+select * from visitors;
